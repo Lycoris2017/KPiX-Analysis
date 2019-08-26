902d20e067fb31dba9465f99b3023724d3ca8dd9
MengqingWu <mengqing.wu@desy.de>
2019-08-26 17:58:57 +0200

    find eudaq2 packages for euDataRead

--- extern/include/euDataRead.h
+++ extern/include/euDataRead.h
@@ -1,3 +1,9 @@
-#include "eudaq/DataConverter.hh"
-#include "eudaq/FileWriter.hh"
-#include "eudaq/FileReader.hh"
+#ifndef __EU_DATAREADER_H__
+
+#define __EU_DATAREADER_H__
+
+//#include "eudaq/DataConverter.hh"
+//#include "eudaq/FileWriter.hh"
+//#include "eudaq/FileReader.hh"
+
+#endif
