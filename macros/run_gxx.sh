a08e70fd45dcd3c3d15eb25e0118bc43041d11b0
Mengqing Wu <wumengtsing@gmail.com>
2020-11-23 14:15:48 +0100

    remove all rootfit plotting to rootfit, if needed, do submodule

--- macros/run_gxx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#g++ -o StandardPlotter StandardPlotter.cxx `root-config --cflags --glibs`
-g++ -o StandardPlotter StandardPlotter.cxx  `root-config --libs --glibs --cflags --ldflags --auxlibs --auxcflags` -L $ROOTSYS/lib -lRooFit -lHtml -lMinuit -lRooFitCore
-
